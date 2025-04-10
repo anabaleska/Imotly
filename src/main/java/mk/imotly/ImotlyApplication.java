cd1933c855fa72e25314da65e4e6cb4f1c3a3bdf
anabaleska <anabaleska5@gmail.com>
2025-04-11 00:34:55 +0200

    Initial commit

---
+++ src/main/java/mk/imotly/ImotlyApplication.java
@@ -0,0 +1,13 @@
+package mk.imotly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImotlyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ImotlyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/mk/imotly/ImotlyApplicationTests.java
@@ -0,0 +1,13 @@
+package mk.imotly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImotlyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
